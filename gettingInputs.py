e0edb1cc5b20220ee044d14076093fce3e63702a
Eren Garipağaoğlu <128929885+ErenGaripagaoglu@users.noreply.github.com>
2023-03-29 13:03:54 +0300

    Add files via upload

---
+++ gettingInputs.py
@@ -0,0 +1,6 @@
+userInput1=str(input("Please write letters or words you want to assign to the string: ")) #you can get input from user with input() method
+print(userInput1)
+print(type(userInput1))
+
+userInput2=int(input("Please write an integer value to assign: "))
+print(type(userInput2))
