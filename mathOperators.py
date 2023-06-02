bfbfd32d7e5167fc82f22bcd2317700b0daca1b8
Eren Garipağaoğlu <128929885+ErenGaripagaoglu@users.noreply.github.com>
2023-06-02 19:28:38 +0300

    Add files via upload

---
+++ mathOperators.py
@@ -0,0 +1,23 @@
+#---------Summation---------#
+
+print(5 + 2)
+
+#--------Subtraction--------#
+
+print(5 - 2)
+
+#-------Multiplication-------#
+
+print(5 * 2)
+
+#-----------Power-----------#
+
+print(5 ** 2)
+
+#----------Division----------#
+
+print(5 / 2)
+
+#----------Division (No Decimal)----------#
+
+print(5 // 2)
