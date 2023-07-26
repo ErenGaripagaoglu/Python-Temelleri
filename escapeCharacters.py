7c3f42270fdc7f2ccc4c3d3e28375532591097ea
Eren Garipağaoğlu <128929885+ErenGaripagaoglu@users.noreply.github.com>
2023-07-26 20:27:07 +0300

    Add files via upload

---
+++ escapeCharacters.py
@@ -0,0 +1,15 @@
+#-----Escape Characters-----#
+
+#----New Line----#
+print("Hello \nWorld!")
+
+#----Tab Space----#
+print("Hello \tWorld!")
+
+#----Backslash----#
+print("C:\\Users\\...")
+
+#----Single Quote----#
+print('Let\'s Go!')
+
+#---------------------------#
